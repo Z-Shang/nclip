9408affe5aec5c192dfe04cd61b61e11ab8022c9
Z.Shang <shangzhanlin@gmail.com>
2021-09-01 01:52:04 +1000

    added linux support via pyperclip

--- rplugin/python3/main.py
+++ rplugin/python3/main.py
@@ -2,18 +2,28 @@ import pynvim
 import pynvim.api.buffer as nvimbuf
 import pynvim.api.window as nvimin
 
-from AppKit import NSPasteboard, NSPasteboardTypeString
+from sys import platform
+
+IS_MAC = False
+if platform == 'darwin':
+    from AppKit import NSPasteboard, NSPasteboardTypeString
+    IS_MAC = True
+else:
+    import pyperclip
 
 @pynvim.plugin
 class NClip:
     def __init__(self, nvim):
         self.nvim = nvim
-        self.pb = NSPasteboard.generalPasteboard()
+        if IS_MAC:
+            self.pb = NSPasteboard.generalPasteboard()
+        else:
+            self.pb = None
         self.buf = None
 
     def prepBuf(self):
         self.buf = self.nvim.api.create_buf(False, True) # as a scratch buf
-        # buf.name = "#<NClipboard>"
+        self.nvim.api.buf_set_option(self.buf, "filetype", "NClipboard")
         return self.buf
         
 
@@ -34,19 +44,34 @@ class NClip:
         }
         buf = self.prepBuf()
         self.nvim.api.open_win(buf, True, opts)
-        self.nvim.api.buf_set_option(buf, "filetype", "NClipboard")
 
     @pynvim.command(name="NClipSession")
     def nclipOnly(self):
         buf = self.prepBuf()
         self.nvim.api.set_current_buf(buf)
-        self.nvim.api.buf_set_option(buf, "filetype", "NClipboard")
+
+    @pynvim.command(name="NClipS")
+    def splits(self):
+        self.nvim.command("bo split")
+        buf = self.prepBuf()
+        win = self.nvim.current.window
+        self.nvim.api.win_set_buf(win, buf)
+
+    @pynvim.command(name="NClipV")
+    def splitv(self):
+        self.nvim.command("bo vsplit")
+        buf = self.prepBuf()
+        win = self.nvim.current.window
+        self.nvim.api.win_set_buf(win, buf)
 
     @pynvim.command(name="NClipWrite")
     def write(self):
         buf = self.buf or self.nvim.current.buffer
         self._setCB(buf)
 
+    @pynvim.command(name="NClipRead")
+    def load(self):
+        self._setBuf(self.nvim.current.buffer)
 
     @pynvim.autocmd('FileType', pattern="NClipboard", allow_nested=True, sync=True)
     def set_buf(self):
@@ -62,8 +87,14 @@ class NClip:
         setToCB(self.pb, content)
 
 def getFromCB(pb):
-    return pb.stringForType_(NSPasteboardTypeString)
+    if IS_MAC:
+        return pb.stringForType_(NSPasteboardTypeString)
+    else:
+        return pyperclip.paste()
 
 def setToCB(pb, content):
-    pb.prepareForNewContentsWithOptions_(1)
-    pb.setString_forType_(content, NSPasteboardTypeString)
+    if IS_MAC:
+        pb.prepareForNewContentsWithOptions_(1)
+        pb.setString_forType_(content, NSPasteboardTypeString)
+    else:
+        pyperclip.copy(content)
